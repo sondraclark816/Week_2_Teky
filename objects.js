d626432ec656aba1514bb1241bf3933dbf09acfd
sondraclark816 <sondraclark@SondraClarkMBP.local>
2016-09-20 10:53:08 -0400

    jQuery Intro

---
+++ objects.js
@@ -0,0 +1,21 @@
+//Object with properties about myself//
+var myObject = {
+    firstName: "Sondra",
+    lastName: "Clark",
+    age: "23",
+    eyeColor:"hazel",
+};
+console.log(myObject);
+
+
+
+//Object Nesting//
+var state = {
+    name:"Kentucky",
+    counties: [
+        {name: ""},
+        {name: "", cites: [
+            {name: ""}
+        ]}
+    ]
+}
